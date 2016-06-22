59ff3f3148364d19c8a2e9309964a60c2fc60a46
R. A. Souza <rasouzas@linux.ime.usp.br>
2016-06-22 17:47:12 -0300

    tenta conexao com LDAP

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,31 @@
+require 'net/ldap'
+
 module API
 	module V1
     class AuthController < Grape::API
       version 'v1'
       format :json
 
+      desc 'Operações de usuário'
       resource :user do
         desc 'Retorna o usuario logado'
         get do
           request.env['REMOTE_USER']
+        end
+
+        desc 'Busca no LDAP'
+        get 'ldap' do
+
+          ldap = Net::LDAP.new
+          ldap.host = '192.168.240.92'
+          ldap.port = 389
+          ldap.auth "cn=admin,dc=linux,dc=ime,dc=usp,dc=br", "123"
+          if ldap.bind
+            print "success"
+          else
+            print "fail"
+          end
+          
         end
       end
 
